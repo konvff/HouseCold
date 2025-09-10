8fc390b0c4e6c713c28486517d48610b724c8596
konvff <bilalsaleh694@gmail.com>
2025-09-10 16:06:22 +0500

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             AdminUserSeeder::class,
-            ServiceTypeSeeder::class,
-            TechnicianAvailabilitySeeder::class,
-            TechnicianServiceTypeSeeder::class,
+            // ServiceTypeSeeder::class,
+            // TechnicianAvailabilitySeeder::class,
+            // TechnicianServiceTypeSeeder::class,
         ]);
     }
 }
